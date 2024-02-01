5661eca006739490c95a524897257323d18810a3
YourRoyalLinus <jonbillings1996@gmail.com>
2024-01-31 20:33:14 -0500

    Clean up spark job

--- src/main/java-11/dev/morling/onebrc/CalculateAverage_YourRoyalLinus_spark.java
+++ src/main/java-11/dev/morling/onebrc/CalculateAverage_YourRoyalLinus_spark.java
@@ -16,6 +16,7 @@
 package dev.morling.onebrc;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
 
 import org.apache.spark.sql.*;
@@ -24,6 +25,18 @@ import static org.apache.spark.sql.functions.*;
 
 public class CalculateAverage_YourRoyalLinus_spark {
     private static final String FILE = "./measurements.txt";
+
+    private static class ResultRow implements Serializable {
+        private String result;
+
+        public String getResult() {
+            return result;
+        }
+
+        public void setResult(String result) {
+            this.result = result;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
@@ -35,9 +48,10 @@ public class CalculateAverage_YourRoyalLinus_spark {
 
         Dataset<Row> input = spark.read().text(FILE);
 
-        Dataset<Row> measurementsDF = input.select(
-                split(col("value"), ";").getItem(0).alias("station"),
-                split(col("value"), ";").getItem(1).cast("Double").alias("measurement"));
+        Dataset<Row> measurementsDF = input
+                .select(
+                        split(col("value"), ";").getItem(0).alias("station"),
+                        split(col("value"), ";").getItem(1).cast("Double").alias("measurement"));
 
         RelationalGroupedDataset groupedMeasurements = measurementsDF.groupBy("station");
 
@@ -45,24 +59,26 @@ public class CalculateAverage_YourRoyalLinus_spark {
         Dataset<Row> minMeasurements = groupedMeasurements.min("measurement");
         Dataset<Row> maxMeasurements = groupedMeasurements.max("measurement");
 
-        Dataset<Row> result = averagedMeasurements.join(
-                minMeasurements, "station").join(
-                        maxMeasurements, "station")
-                .orderBy("station").select(
-                        concat(
-                                col("station"), lit("="),
+        Encoder<ResultRow> resultRowEncoder = Encoders.bean(ResultRow.class);
+        Dataset<ResultRow> result = averagedMeasurements
+                .join(minMeasurements, "station")
+                .join(maxMeasurements, "station")
+                .orderBy("station")
+                .select(
+                        concat(col("station"), lit("="),
                                 round(col("min(measurement)"), 1), lit("/"),
                                 round(col("avg(measurement)"), 1), lit("/"),
-                                round(col("max(measurement)"), 1)).alias("output"));
+                                round(col("max(measurement)"), 1)).alias("result"))
+                .as(resultRowEncoder);
 
         StringBuilder output = new StringBuilder("{");
 
-        List<Row> results = result.collectAsList();
+        List<ResultRow> results = result.collectAsList();
         for (int i = 0; i < results.size() - 1; i++) {
-            output.append(results.get(i).getString(0) + ", ");
+            output.append(results.get(i).getResult() + ", ");
         }
 
-        output.append(results.get(results.size() - 1).getString(0));
+        output.append(results.get(results.size() - 1).getResult());
         output.append("}");
 
         System.out.println(output);
