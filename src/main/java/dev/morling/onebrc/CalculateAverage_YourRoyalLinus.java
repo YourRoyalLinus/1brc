4eab0f5c1780c84d9943d348f5bc79be1c700c27
YourRoyalLinus <jonbillings1996@gmail.com>
2024-02-18 18:06:04 -0500

    Clean up from rebase

--- src/main/java/dev/morling/onebrc/CalculateAverage_YourRoyalLinus.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_YourRoyalLinus.java
@@ -30,98 +30,25 @@ import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CalculateAverage_YourRoyalLinus {
+    /*
+     * This solution was heavily inspired by CalculateAverage_spullara
+     *
+     * * My results on this computer:
+     *
+     * CalculateAverage_spullara: 0m2.013s
+     * CalculateAverage_YourRoyalLinus: 0m26.692s
+     * CalculateAverage: 3m22.363s
+     *
+     */
+
     private static final String FILE = "./measurements.txt";
     private static final ConcurrentHashMap<String, ResultRecord> concurrentMap = new ConcurrentHashMap<>();
-
-    private static final int BUFFER_SIZE = 10000;
-
-    private static record Measurement(String station, double value) {
-        private Measurement(String[] parts) {
-            this(parts[0], Double.parseDouble(parts[1]));
-        }
-    }
-
-    private static record ResultRow(double min, double mean, double max) {
-
-        public String toString() {
-            return round(min) + "/" + round(mean) + "/" + round(max);
-        }
-
-        private double round(double value) {
-            return Math.round(value * 10.0) / 10.0;
-        }
-    };
-
-    private static class MeasurementAggregator {
-        private double min = Double.POSITIVE_INFINITY;
-        private double max = Double.NEGATIVE_INFINITY;
-        private double sum;
-        private long count;
-    }
-
-    private static class Aggregator implements Runnable {
-        private final List<String> input;
-
-        private Map<String, MeasurementAggregator> aggregatedResult;
-
-        Aggregator(String[] lines) {
-            input = Arrays.asList(lines);
-        }
-
-        @Override
-        public void run() {
-            Collector<Measurement, MeasurementAggregator, MeasurementAggregator> collector = Collector.of(
-                    MeasurementAggregator::new,
-                    (a, m) -> {
-                        a.min = Math.min(a.min, m.value);
-                        a.max = Math.max(a.max, m.value);
-                        a.sum += m.value;
-                        a.count++;
-                    },
-                    (agg1, agg2) -> {
-                        var res = new MeasurementAggregator();
-                        res.min = Math.min(agg1.min, agg2.min);
-                        res.max = Math.max(agg1.max, agg2.max);
-                        res.sum = agg1.sum + agg2.sum;
-                        res.count = agg1.count + agg2.count;
-
-                        return res;
-                    },
-                    agg -> {
-                        var res = new MeasurementAggregator();
-                        res.min = agg.min;
-                        res.max = agg.max;
-                        res.sum = agg.sum;
-                        res.count = agg.count;
-
-                        return res;
-                    });
-
-            aggregatedResult = input.stream()
-                    .map(l -> new Measurement(l.split(";")))
-                    .collect(groupingBy(Measurement::station, collector));
-        }
-
-        public Map<String, MeasurementAggregator> getResult() {
-            return aggregatedResult;
-        }
-    }
-
-    private static String[] getNextBatch(Scanner scanner) {
-        String[] lines = new String[BUFFER_SIZE];
-        int i = 0;
-        while (scanner.hasNextLine() && i < BUFFER_SIZE) {
-            lines[i++] = scanner.nextLine();
-        }
-
-        return lines;
-    }
 
     public static void main(String[] args) throws IOException {
         File file = new File(FILE);
         long start = System.currentTimeMillis();
 
-        getFilePartitions(file).stream().parallel().forEach(partition -> {
+        getFilePartitions(file).parallelStream().forEach(partition -> {
             long partitionEnd = partition.end();
             try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(Path.of(FILE), StandardOpenOption.READ)) {
                 MappedByteBuffer byteBuf = fileChannel.map(FileChannel.MapMode.READ_ONLY, partition.start(), partitionEnd - partition.start());
